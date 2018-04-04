0cf5b6edb68da690d571b969cadeffb25e15816b
ljkh <cawatou@gmail.com>
2018-04-04 23:16:48 +0300

    grade fix + prod mode

--- public_html/index.php
+++ public_html/index.php
@@ -3,11 +3,11 @@ ini_set("display_errors",1);
 error_reporting(E_ALL);
 // comment out the following two lines when deployed to production
 // DEV mode
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+//defined('YII_DEBUG') or define('YII_DEBUG', true);
+//defined('YII_ENV') or define('YII_ENV', 'dev');
 
 // PROD mode
-//defined('YII_ENV') or define('YII_ENV', 'prod');
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
